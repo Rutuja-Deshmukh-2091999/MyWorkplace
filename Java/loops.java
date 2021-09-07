f126bf4ad7afedf22bc00557165779a952900e0c
Rutuja-Deshmukh-2091999 <84307464+Rutuja-Deshmukh-2091999@users.noreply.github.com>
2021-09-07 20:01:40 +0530

    Add files via upload

---
+++ Java/loops.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        boolean isonrepeat = true;
+        while(isonrepeat){
+            System.out.println("Playing your song om repeat");
+            System.out.println("Do you want to exit ?");
+
+            String a = s.nextLine();
+            if (a.equals("yes"))
+            {
+                isonrepeat = false;
+            }
+
+
+        }
+        System.out.println("Enjoy your next song");
+
+    }
+}
