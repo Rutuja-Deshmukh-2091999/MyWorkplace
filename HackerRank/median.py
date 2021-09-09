f3a97fff7bc03464cbff9d4b601ecffe8a46a70e
Rutuja-Deshmukh-2091999 <84307464+Rutuja-Deshmukh-2091999@users.noreply.github.com>
2021-09-09 13:06:09 +0530

    Add files via upload

---
+++ HackerRank/median.py
@@ -0,0 +1,10 @@
+import sys
+def findMedian(arr):
+    arr = sorted(arr)
+    return arr[len(arr)//2]
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    arr = list(map(int, input().strip().split(' ')))
+    result = findMedian(arr)
+    print(result)
