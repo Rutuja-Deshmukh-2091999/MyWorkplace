998382ad32cc1651ff9a6f3975bee66f0a6e1829
Rutuja-Deshmukh-2091999 <84307464+Rutuja-Deshmukh-2091999@users.noreply.github.com>
2021-12-17 11:02:17 +0530

    Add files via upload

---
+++ Python/swapping.py
@@ -0,0 +1,5 @@
+num1 = int(input())
+num2 = int(input())
+print("Numbers before swapping","num1 -",num1,", num2 -",num2)
+num1,num2 = num2,num1
+print("Numbers after swapping","num1 -",num1,", num2 -",num2)
