1051786be97b72028e64d067a423174bdcedb1ab
Rutuja-Deshmukh-2091999 <84307464+Rutuja-Deshmukh-2091999@users.noreply.github.com>
2022-02-03 10:07:35 +0530

    Delete sample3.py

--- Python/sample3.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import xlwings as xw
-
-PATH,password1 = r"C:\Users\User\Desktop\EXCEL\1338-01052021.xlsx","C06210"
-wb = xw.Book(PATH,password1)
-sheet = wb.sheets['1338-01052021']
-
-df = sheet['A1:C4'].options(pd.DataFrame, index=False, header=True).value
-print(df)
